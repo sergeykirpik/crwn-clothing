e0dd188e60699a426cced1ee52e6ff4c094d67dc
Sergey Kirpik <sergeykirpik@gmail.com>
2019-12-10 00:28:34 +0200

    adding header component and links to home, shop page and contact

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -15,7 +15,6 @@ class ShopPage extends React.Component {
     const { collections } = this.state;
     return (
       <div className="shop-page">
-        <h1>SHOP PAGE</h1>
         {collections.map(({ id, ...collectionProps }) => (
           <CollectionPreview key={id} {...collectionProps} />
         ))}
